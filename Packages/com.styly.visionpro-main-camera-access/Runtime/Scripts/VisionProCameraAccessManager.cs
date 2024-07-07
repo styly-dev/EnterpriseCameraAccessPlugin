6ef3222e612a042142f27e4ad5f38821732ecd85
Masahiro Yamaguchi <from2001@hotmail.com>
2024-07-07 23:28:56 +0900

    Added code for PicoXR

--- Packages/com.styly.visionpro-main-camera-access/Runtime/Scripts/VisionProCameraAccessManager.cs
+++ Packages/com.styly.visionpro-main-camera-access/Runtime/Scripts/VisionProCameraAccessManager.cs
@@ -3,6 +3,13 @@ using AOT;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using UnityEngine.UI;
+using System;
+
+
+#if USE_PICOXR && UNITY_ANDROID && !UNITY_EDITOR
+using Unity.XR.PICO.TOBSupport;
+using Unity.XR.PXR;
+#endif
 
 public class VisionProCameraAccessManager : MonoBehaviour
 {
@@ -16,6 +23,11 @@ public class VisionProCameraAccessManager : MonoBehaviour
     private Texture2D tmpTexture = null;
     private string tempBase64String = null;
     private float skipSeconds = 0.1f;
+
+#if USE_PICOXR && UNITY_ANDROID && !UNITY_EDITOR
+    private int PicoImageWidth = 1164;
+    private int PicoImageHeight = 874;
+#endif
 
     /// <summary>
     /// Get Vision Pro main camera image as texture2D.
@@ -42,12 +54,17 @@ public class VisionProCameraAccessManager : MonoBehaviour
     void OnEnable()
     {
 #if UNITY_VISIONOS && !UNITY_EDITOR
-                SetNativeCallbackOfCameraAccess(CallbackFromNative);
+        SetNativeCallbackOfCameraAccess(CallbackFromNative);
 #endif
     }
 
     void Start()
     {
+
+#if USE_PICOXR && UNITY_ANDROID && !UNITY_EDITOR
+        PicoStart();
+        return;
+#endif
 
 #if UNITY_VISIONOS && !UNITY_EDITOR
         // Start the main camera capture
@@ -73,14 +90,16 @@ public class VisionProCameraAccessManager : MonoBehaviour
 
     void OnDisable()
     {
-        if (IsVisionOs())
-        {
-            SetNativeCallbackOfCameraAccess(null);
-        }
-        else
-        {
-            if (webCamTexture != null) { webCamTexture.Stop(); }
-        }
+#if USE_PICOXR && UNITY_ANDROID && !UNITY_EDITOR
+        OnPicoDisable();
+        return;
+#endif
+
+#if UNITY_VISIONOS && !UNITY_EDITOR
+        SetNativeCallbackOfCameraAccess(null);
+        return;
+#endif
+        if (webCamTexture != null) { webCamTexture.Stop(); }
     }
 
     IEnumerator RequestCameraPermission_iOS()
@@ -118,6 +137,10 @@ public class VisionProCameraAccessManager : MonoBehaviour
     {
         // Apply WebCamTexture to material
         ApplyWebcamTextureToMaterial(PreviewMaterial, webCamTexture);
+
+#if USE_PICOXR && UNITY_ANDROID && !UNITY_EDITOR
+        ApplyPicoFrameToMaterial(PreviewMaterial);
+#endif
     }
 
     void StartWebCam(string deviceName)
@@ -151,8 +174,8 @@ public class VisionProCameraAccessManager : MonoBehaviour
     void ApplyBase64StringToMaterial(Material material, string base64String)
     {
         if (base64String == null) { return; }
-        Texture2D tempTexture = Base64ToTexture2D(base64String);
-        material.mainTexture = tempTexture;
+        tmpTexture = Base64ToTexture2D(base64String);
+        material.mainTexture = tmpTexture;
     }
 
     // Convert Base64String to Texture2D
@@ -194,4 +217,77 @@ public class VisionProCameraAccessManager : MonoBehaviour
     static void StartVisionProMainCameraCapture() { }
 #endif
 
+
+#if USE_PICOXR && UNITY_ANDROID && !UNITY_EDITOR
+    // Code for PicoXR
+
+    private void PicoStart()
+    {
+        PXR_Boundary.EnableSeeThroughManual(true);
+        PXR_Enterprise.InitEnterpriseService();
+        PXR_Enterprise.BindEnterpriseService();
+
+        tmpTexture = new Texture2D(PicoImageWidth, PicoImageHeight, TextureFormat.RGB24, false, false);
+        OpenVSTCamera();
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            PXR_Boundary.EnableSeeThroughManual(true);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseVSTCamera();
+    }
+
+    private void OnPicoDisable()
+    {
+        tmpTexture = null;
+        PXR_Enterprise.CloseVSTCamera();
+    }
+
+    public void OpenVSTCamera()
+    {
+        bool result = PXR_Enterprise.OpenVSTCamera();
+        Debug.Log("Open VST Camera" + result);
+    }
+
+    public void CloseVSTCamera()
+    {
+        bool result = PXR_Enterprise.CloseVSTCamera();
+        Debug.Log("Close VST Camera" + result);
+    }
+
+    void ApplyPicoFrameToMaterial(Material material)
+    {
+        try
+        {
+            // Acquire the camera frame from PicoXR
+            PXR_Enterprise.AcquireVSTCameraFrameAntiDistortion(PicoImageWidth, PicoImageHeight, out Frame frame);
+            tmpTexture.LoadRawTextureData(frame.data, (int)frame.datasize);
+            tmpTexture.Apply();
+
+            // Flip the image vertically
+            Color[] pixels = tmpTexture.GetPixels();
+            Color[] flippedPixels = new Color[pixels.Length];
+            int width = tmpTexture.width;
+            int height = tmpTexture.height;
+            for (int y = 0; y < height; y++) { for (int x = 0; x < width; x++) { flippedPixels[x + y * width] = pixels[x + (height - y - 1) * width]; } }
+            tmpTexture.SetPixels(flippedPixels);
+            tmpTexture.Apply();
+
+            // Apply to material
+            material.mainTexture = tmpTexture;
+        }
+        catch (Exception e)
+        {
+            Debug.LogFormat("e={0}", e);
+            throw;
+        }
+    }
+#endif
 }
